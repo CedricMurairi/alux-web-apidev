03a828f08e047cc580bb117a97ecf894e9ea8c3f
Mthabisi Ndlovu <m.ndlovu@alustudent.com>
2021-03-17 23:35:19 +0200

    Debugging: Edited files to fix build issues.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import membersRouter from "./membersRouter.js"
+import membersRouter from "./membersRouter.js";
+import borrowingsRouter from "./borrowingsRouter.js";
 
 const router = express.Router();
 
 
 router.use("/members", membersRouter);
+router.use("/borrow", borrowingsRouter);
 
 
 export default router;
